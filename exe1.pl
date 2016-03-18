90ae95b024ea9d6d7761b2a30bc9eb721037124f
Célia Figueiredo <a67637@alunos.uminho.pt>
2016-03-18 20:06:28 +0000

    está quase
    vamos lá

--- exe1.pl
+++ exe1.pl
@@ -10,7 +10,8 @@
 :-dynamic instituicao/1.
 :-dynamic servico/1.
 :-dynamic profissional/1.
-:-dynamic instserv/2, utenserv/2, utentinst/2. 
+:-dynamic instserv/2.
+:-dynamic utenserv/2, utentinst/2. 
 :-dynamic ok/3.
 
 
@@ -86,26 +87,98 @@ utentinst( jose, hospitalBraga ).
 utentinst( joana, hospitalBeatrizAngelo ). 
 utentinst( manuel, hospitalLisboaNorte ). 
 
--------------------------- - - - - - - - - - -  -  -  -  -   -
+%-------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado utenserv(utente,servico).
 
-utenserv( jose, endocrinologia ). 
-utenserv( maria, cardiologia ).
+utenserv( jose,endocrinologia). 
+utenserv(jose,cardiologia).
+utenserv( maria,cardiologia ).
 utenserv( carlos, urologia ).  
 utenserv( joana, endocrinologia ).  
 utenserv( manuel, neurologia ).  
 
+%-------------------------- - - - - - - - - - -  -  -  -  -   -
+printLista([]):-write('').
+printLista([H|T]):-
+	write(H),nl,
+	printLista(T).
 
--------------------------- - - - - - - - - - -  -  -  -  -   -
+%-------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado concat:(Lista,Lista,Lista)->{V,F}
+
+concat([],L,L).
+concat([X|L1],L2,[X|L3]):- concat(L1,L2,L3).
+
+
+
+%-------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado nao
+nao( Questao) :-
+	Questao, !,fail.
+nao(Questao).
+
+
+%-------------------------- - - - - - - - - - -  -  -  -  -   -
+difList([],_,[]).
+difList([H1|T1],L2,[H1|L3]):-
+	nao(member(H1,L2)), difList(T1,L2,L3).
+difList([_|T1],L2,L3):-
+	difList(T1,L2,L3).
+
+
+%-------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado utenservinst:(utente,servico, instituicao)->{V,F}
 
-ok(X,Y,Z):-
-    utenserv(X, Y),
-    utentinst(X,Z),
-    instserv(Z,Y). 
-         
-         
+utenservinst(X,Y,Z):-
+     utenserv(X,Y),
+     utentinst(X,Z),
+     instserv(Z,Y),
+     utente(X),
+     instituicao(Z),
+     servico(Y).
+%-------------------------- - - - - - - - - - -  -  -  -  -   -
+% Identificar as instituições onde seja prestado um dado serviço ou conjunto de serviços;
 
+% Extensao do predicado instServico: ([Serviço],[Instituição])->{V,F}    
+ 
+instServico(Serv,I):-    
+	findall(H,instserv(H,Serv),I).  /* vai procurar os hospitais que tenham um dado servico e guarda-os na lista I    */ 
+         
+instServicos([],[]).   
+instServicos([S|T],I):-    
+	instServico(S,I1),   
+	instServicos(T,I2),concat(I1,I2,I).
+         
+         
+%--------------------------- --- - - - -- - -- - - - -  - -- - - -
+
+% Indentificar os serviços existentes numa instituicao
+
+% Extensao do predicado existeServInst : [Serviço],Instituição->{V,F}
+existeServInst(Serv,I):- 
+                       findall(K,instserv(I,K),Serv). 
+         
+         
+         
+%-------------------------- - - - - - - - - - -  -  -  -  -   -         
+% Identificar os serviços que não se podem encontrar numa instituição;
+
+% Extensao do predicado servicosForaInst : Instituicao,Serviço->{V,F}
+
+ todosServicos(L):-
+	findall(S,servico(S),L).
+
+servicosForaInst(Ins,Serv):-todosServicos(P), 
+                            existeServInst(K,Ins),
+                            difList(P,K,Serv).
+                             
+                             
+                             
+                             
+                                   
+
+ 
+         
 
 
 
